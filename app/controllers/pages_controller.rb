1c7034c5eb23fb9503751445877b23d003a9a8fd
Allan Victor <doislan@gmail.com>
2017-01-15 16:40:41 -0200

    Make API request to show player data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
 	def show
 		render template: "pages/#{params[:page]}"
 	end
+
+	def search
+		render template: "player/#{params[:page]}"
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
+require 'net/http'
+
 module ApplicationHelper
+	
+	QUICKPLAY_URL = 'https://api.lootbox.eu/pc/us/[battletag]/quickplay/allHeroes/'
+	COMPETITIVE_URL = 'https://api.lootbox.eu/pc/us/[battletag]/competitive/allHeroes/'
+	SEPARATOR_CHAR = "%23"
+
+	def getCompetitivePlayerData(battletag)
+		player_request = getUrlRequest
+
+		return getData(player_request)
+	end
+
+	def getUrlRequest(battletag)
+		normalized_tag = normalizeBattleTag(battletag)
+
+		return COMPETITIVE_URL.sub("[battletag]", normalized_tag)
+	end
+
+	def normalizeBattleTag(battletag)
+		return battletag.sub("#", SEPARATOR_CHAR)
+	end
+
+	def getData(url_request)
+		url = URI.parse(url_request)
+		request = Net::HTTP::Get.new(url.to_s)
+		response = Net::HTTP.start(url.host, url.port) {|http|
+		  http.request(request)
+		}
+		
+		return response.body
+	end
+	
 end
